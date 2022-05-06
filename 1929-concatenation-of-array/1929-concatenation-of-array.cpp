52a9c86203ce9c0dda6245f84d4789cea328687e
HAIL CODING <86044203+HAILCODING444@users.noreply.github.com>
2022-05-06 13:23:29 +0530

    Time: 8 ms (83.70%), Space: 12.9 MB (40.71%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -0,0 +1,12 @@
+// time complexity o(n)
+// space compexity o(1)
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size() ;
+        for(int i=0 ; i<n ; i++){
+            nums.push_back(nums[i]) ; 
+        }
+        return nums ; 
+    }
+};
