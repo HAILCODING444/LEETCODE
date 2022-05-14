21d84ccb123cf7128d5de6de868a8380839341bd
HAIL CODING <86044203+HAILCODING444@users.noreply.github.com>
2022-05-15 00:12:31 +0530

    Time: 8 ms (80.46%), Space: 10.1 MB (61.26%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size() ; 
+        priority_queue<int , vector<int> , greater<int>> q ; 
+        
+        for(int i : nums){
+            q.push(i) ; 
+            if(q.size() > k){
+                q.pop() ; 
+            }
+        }
+        return q.top() ; 
+    }
+};
